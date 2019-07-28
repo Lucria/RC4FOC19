eaea46c9ec3c1cf05174b3ff38a4253ef4e92f5a
Lucria <jerryzhang28eclipse@gmail.com>
2019-07-28 13:20:33 +0800

    Testing firebase

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ function testFunction() {
     // Functin tested working
     const myElement = document.getElementById("square8");
     myElement.style.backgroundColor = "#FF00FF";
+    var database = firebase.database();
+    writeUserData("square1", "red");
+    console.log("DONE");
+}
+
+function writeUserData(squareID, colour) {
+    firebase.database().ref('bases/' + squareID).set({
+        currentColour: colour,
+    });
 }
 
 function updateBase(squareID, colour) {
     const targetBase = document.getElementById(squareID);
     targetBase.style.backgroundColor = colour;
 }
+
+var colourListener = firebase.database().ref("bases/square1/currentColour");
+colourListener.on('value', function (snapshot) {
+    updateBase(postElement, snapshot.val());
+});
 
 var interval = setInterval(function () {
     const squareID = "Placeholder";
