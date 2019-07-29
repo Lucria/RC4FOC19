24577573f54dca1468c8d48b7dad7225c7481ec0
Lucria <jerryzhang28eclipse@gmail.com>
2019-07-30 01:55:07 +0800

    Cleaned up Repo

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-function testFunction() {
-    // Functin tested working
-    const myElement = document.getElementById("square1");
-    myElement.style.backgroundColor = "#FF00FF";
-    var database = firebase.database();
-    writeUserData("square1", "cyan");
-    console.log("DONE");
-}
-
-function writeUserData(squareID, colour) {
-    firebase.database().ref('bases/' + squareID).set({
-        currentColour: colour,
-    });
-}
-
 function updateBase(squareID, colour) {
     const targetBase = document.getElementById(squareID);
     targetBase.style.backgroundColor = colour;
 }
 
-var colourListener = firebase.database().ref("bases/square1/currentColour");
-colourListener.on('value', function (snapshot) {
-    updateBase(postElement, snapshot.val());
-});
 
-var interval = setInterval(function () {
-    const squareID = "Placeholder";
-    const colour = "Placeholder";
-    updateBase(squareID, colour);
-}, 5000);
+let n = 20;
+
+for (let i = 1; i <= n; i++) {
+    const element = "square" + i;
+    var colourListener = firebase.database().ref("bases/" + element + "/currentColour");
+    colourListener.on('value', function (snapshot) {
+        updateBase(element, snapshot.val());
+    });
+}
+
+
